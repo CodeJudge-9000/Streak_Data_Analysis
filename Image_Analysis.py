8717473e75a3ee22b63cf0937eec3994c486af89
CodeJudge-9000 <66952128+CodeJudge-9000@users.noreply.github.com>
2020-12-30 01:39:46 +0100

    Update Image_Analysis.py
    Fixed code (multiple errors) and seperated brownian motion from main file.

--- Image_Analysis.py
+++ Image_Analysis.py
@@ -12,9 +12,7 @@ get_ipython().magic('reset -sf')
 import numpy as np
 import scipy.ndimage
 import matplotlib.pyplot as plt
-#import scipy
 import cv2
-#from PIL import image
 import skimage.color
 import skimage.filters
 import skimage.viewer
@@ -23,7 +21,8 @@ import skvideo.io
 import os
 import gc
 import math as m
-import csv
+import statistics as stat
+from itertools import compress
 
 
 #Change working directory
@@ -349,7 +348,7 @@ def viewStreaks(list1,columns):
     fig=plt.figure(figsize=(8, 8))
     # Number of columns and rows in the final figure
     columns = 4
-    rows = math.ceil(len(list1)/columns)
+    rows = m.ceil(len(list1)/columns)
     
     # Loop that adds each image to the final figure and displays it
     for k in range(1,len(list1)+1):
@@ -374,7 +373,7 @@ def acceptedStreaks(list1,boolean,columns):
             streaks.append(list1[i-1])
            
     # Calculating number of rows needed
-    rows = math.ceil(len(streaks)/4)
+    rows = m.ceil(len(streaks)/4)
     
     # Loop that adds each image to the final figure and displays it
     for k in range(1,len(streaks)+1):
@@ -399,7 +398,7 @@ def declinedStreaks(list1,boolean,columns):
             streaks.append(list1[i-1])
     
     # Calculating number of rows needed
-    rows = math.ceil(len(streaks)/4)
+    rows = m.ceil(len(streaks)/4)
     
     # Loop that adds each image to the final figure and displays it
     for k in range(1,len(streaks)+1):
@@ -408,7 +407,10 @@ def declinedStreaks(list1,boolean,columns):
         plt.suptitle('Declined Streaks')
         plt.show()
 
-     
+
+# Actual Image Processing
+# Just type in whatever you need underneath
+
 greyedVid=loadDataAsGrey("0DC- AC 50 Hz 0.7V WE3 CERE24.avi",0,101,300)
 greyedVid=rotate(greyedVid,0,86,341,78,"h")
 noAvg=removeaverage(greyedVid)
@@ -429,73 +431,5 @@ streakLengths=streakLength(Frames,132)
 lenmean=stat.mean(streakLengths[0])
 lenStd=stat.pstdev(streakLengths[0])
         
-acceptedStreaks(liste,test,4)
-declinedStreaks(liste,test,4)
-
-## Trajectory plot of Brownian motion by Johanna
-
-# Changes working directory
-os.chdir('/Users/Johanna/OneDrive - Danmarks Tekniske Universitet/DTU/3. Semester/Fagprojekt/Data 27112020/Brownian motion_after60min')
-
-# Empty lists for the trajectory number and the coordinates
-trajec = []
-x = []
-y = []
-
-# Reads the csv file
-with open('Trajectories2.csv', 'r') as rf:
-    reader = csv.reader(rf, delimiter=',')
-    # Appends the trajectory number and the coordinates to the empty lists
-    for row in reader:
-        trajec.append(row[1])
-        x.append(row[3])
-        y.append(row[4])
-
-# Deletes the first element in the lists, which is the title of the category
-del trajec[0]
-del x[0]
-del y[0]
-
-# Converts strings to float in list
-new_trajec = [float(i) for i in trajec]
-
-# Empty list for the indices of different trajectories
-indices = []
-
-# Loop to find where one trajectory ends and another begins
-# The appended value is the last index before a new trajectory begins
-for j in range(0, len(new_trajec)-1):
-    if new_trajec[j]-new_trajec[j+1] != 0:
-        indices.append(j)
-
-# The number 0 is added to the list of indices
-indices.insert(0, 0)
-
-# Converts strings to float in lists
-new_x = [float(i) for i in x]
-new_y = [float(i) for i in y]
-
-# Makes sure that each trajectory gets a different color
-def get_cmap(n, name='hsv'):
-    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
-    RGB color; the keyword argument name must be a standard mpl colormap name.'''
-    return plt.cm.get_cmap(name, n)
-
-# Makes sure that each trajectory gets a different color
-N = len(indices)-1
-cmap = get_cmap(N)
-
-# Loop that plots each trajectory based on the indices 
-for k in range(0, len(indices)-1):
-    x2 = new_x[indices[k]+1:indices[k+1]+1]
-    x3 = [x * 0.132 for x in x2]
-    y2 = new_y[indices[k]+1:indices[k+1]+1]
-    y3 = [y * 0.132 for y in y2]
-    #print(x2)
-    #print(y2)
-    if len(x2) >= 10 and len(y2) >= 10:
-        plt.plot(x3, y3, '--', c=cmap(k))
-        plt.title("Particle Trajectory due to Brownian Motion")
-        plt.xlabel('x [\u03BCm]')
-        plt.ylabel('y [\u03BCm]')
-        plt.show()
+#acceptedStreaks(liste,test,4)
+#declinedStreaks(liste,test,4)
